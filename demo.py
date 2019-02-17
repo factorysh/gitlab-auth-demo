186e06464b1ce5b2327bee4d25aa23789bf2a871
Mathieu Lecarme <mlecarme@bearstech.com>
2019-02-17 13:53:21 +0100

    More violence

--- demo.py
+++ demo.py
@@ -4,6 +4,7 @@ import urllib.parse
 
 import requests
 from authlib.specs.rfc7519 import jwt
+from x5092json import x509parser
 
 
 class Gitlab:
@@ -29,9 +30,21 @@ class Gitlab:
         return r.json().get('token')
 
 
+def public_key(path):
+    "Get a PEM path, return a PEM public key"
+    cert = x509parser.load_certificate(open(path, 'rb'))
+    cc = x509parser.parse(cert)
+    public = cc['subject_public_key_info']
+    assert public['algorithm'] == 'rsaEncryption'
+    assert public['key_size'] >= 2048
+    return """-----BEGIN PUBLIC KEY-----
+%s
+-----END PUBLIC KEY-----""" % public['key']
+
+
 def assert_token(raw, public="public.pem"):
     "Read token and validate it"
-    pk = open(public, 'rb').read()
+    pk = public_key(public)
     t = jwt.decode(raw, pk)
     h = t.header
     assert h['typ'] == 'JWT'
@@ -44,8 +57,8 @@ def assert_token(raw, public="public.pem"):
 
 if __name__ == '__main__':
     g = Gitlab(os.getenv('GITLAB'))
-    raw = g.token(os.getenv('PROJECT'))
+    raw = g.token(os.getenv('PROJECT'), client_id='bob')
     print(raw)
     print()
-    t = assert_token(raw)
+    t = assert_token(raw, public='toto.pem')
     print(t)
